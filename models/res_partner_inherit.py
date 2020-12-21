ad62cbbf2413c313e182967d7240a690356872cf
planet-odoo <sailee.haryan@planet-odoo.com>
2020-12-21 17:34:24 +0530

    final changes made

--- models/res_partner_inherit.py
+++ models/res_partner_inherit.py
@@ -12,10 +12,6 @@ class ResPartnerInherit(models.Model):
     sale_detail_ids = fields.One2many('website.sale.detail', 'res_partner_id', string='Sale Details')
 
 
-
-
-
-
 class WebsiteSaleDetail(models.Model):
     _name= 'website.sale.detail'
 
@@ -23,5 +19,7 @@ class WebsiteSaleDetail(models.Model):
     product_tree_name = fields.Many2one('product.product', string='Tree')
     is_gift = fields.Boolean('Is Gift')
     qty = fields.Integer('Quantity')
+    sale_id = fields.Many2one('sale.order','Sale Order')
+    payment_id = fields.Many2one('payment.transaction','Transaction Id')
 
     res_partner_id = fields.Many2one('res.partner', 'Partner_id')
